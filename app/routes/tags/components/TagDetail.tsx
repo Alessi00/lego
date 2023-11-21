webkom/lego-webapp

Refactor tags route to `react-router` v6

app/routes/tags/components/TagDetail.tsx
@@ -1,54 +1,61 @@
 import { Card, Flex } from '@webkom/lego-bricks';
 import { map, toPairs } from 'lodash';
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom-v5-compat';
+import { fetch } from 'app/actions/TagActions';
 import { Content } from 'app/components/Content';
+import { selectTagById } from 'app/reducers/tags';
+import { useAppDispatch, useAppSelector } from 'app/store/hooks';
 import styles from './TagDetail.css';
 
-type Props = {
-  tag: Record<string, any>;
+const transelate = (key: string) => {
+  const trans = {
+    article: 'Artikler',
+    event: 'Arrangementer',
+    quote: 'Sitater',
+    joblisting: 'Jobbannonser',
+  };
+  return trans[key];
 };
 
-class TagDetail extends Component<Props> {
-  transelate = (key: string) => {
-    const trans = {
-      article: 'Artikler',
-      event: 'Arrangementer',
-      quote: 'Sitater',
-      joblisting: 'Jobbannonser',
-    };
-    return trans[key];
+const link = (key: string, tag: string) => {
+  const links = {
+    article: (
+      <Link to={`/articles?tag=${tag}`}>
+        <h4>{transelate(key)}</h4>
+      </Link>
+    ),
   };
-  link = (key: string, tag: string) => {
-    const links = {
-      article: (
-        <Link to={`/articles?tag=${tag}`}>
-          <h4>{this.transelate(key)}</h4>
-        </Link>
-      ),
-    };
-    return links[key] || <h4>{this.transelate(key)}</h4>;
-  };
+  return links[key] || <h4>{transelate(key)}</h4>;
+};
 
-  render() {
-    const { tag } = this.props;
-    return (
-      <Content>
-        <Helmet title={tag.tag} />
-        <h1>{tag.tag}</h1>
+const TagDetail = () => {
+  const { tagId } = useParams();
+  const tag = useAppSelector((state) => selectTagById(state, { tagId }));
 
-        <Flex wrap>
-          {map(toPairs(tag.relatedCounts), (pair) => (
-            <Card className={styles.entity}>
-              <h2>{pair[1]}</h2>
-              {this.link(pair[0], tag.tag)}
-            </Card>
-          ))}
-        </Flex>
-      </Content>
-    );
-  }
-}
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetch(tagId));
+  }, [dispatch, tagId]);
+
+  return (
+    <Content>
+      <Helmet title={tag.tag} />
+      <h1>{tag.tag}</h1>
+
+      <Flex wrap>
+        {map(toPairs(tag.relatedCounts), (pair) => (
+          <Card className={styles.entity}>
+            <h2>{pair[1]}</h2>
+            {link(pair[0], tag.tag)}
+          </Card>
+        ))}
+      </Flex>
+    </Content>
+  );
+};
 
 export default TagDetail;

Alessi00/lego

app/routes/tags/components/TagDetail.tsx
@@ -1,54 +1,62 @@
 import { Card, Flex } from '@webkom/lego-bricks';
 import { map, toPairs } from 'lodash';
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom-v5-compat';
+import { fetch } from 'app/actions/TagActions';
 import { Content } from 'app/components/Content';
+import { selectTagById } from 'app/reducers/tags';
+import { useAppDispatch, useAppSelector } from 'app/store/hooks';
 import styles from './TagDetail.css';
 
-type Props = {
-  tag: Record<string, any>;
+const translate = (key: string) => {
+  const trans = {
+    article: 'Artikler',
+    event: 'Arrangementer',
+    quote: 'Sitater',
+    joblisting: 'Jobbannonser',
+    poll: 'Avstemninger',
+  };
+  return trans[key];
 };
 
-class TagDetail extends Component<Props> {
-  transelate = (key: string) => {
-    const trans = {
-      article: 'Artikler',
-      event: 'Arrangementer',
-      quote: 'Sitater',
-      joblisting: 'Jobbannonser',
-    };
-    return trans[key];
+const link = (key: string, tag: string) => {
+  const links = {
+    article: (
+      <Link to={`/articles?tag=${tag}`}>
+        <h4>{translate(key)}</h4>
+      </Link>
+    ),
   };
-  link = (key: string, tag: string) => {
-    const links = {
-      article: (
-        <Link to={`/articles?tag=${tag}`}>
-          <h4>{this.transelate(key)}</h4>
-        </Link>
-      ),
-    };
-    return links[key] || <h4>{this.transelate(key)}</h4>;
-  };
+  return links[key] || <h4>{translate(key)}</h4>;
+};
 
-  render() {
-    const { tag } = this.props;
-    return (
-      <Content>
-        <Helmet title={tag.tag} />
-        <h1>{tag.tag}</h1>
+const TagDetail = () => {
+  const { tagId } = useParams();
+  const tag = useAppSelector((state) => selectTagById(state, { tagId }));
 
-        <Flex wrap>
-          {map(toPairs(tag.relatedCounts), (pair) => (
-            <Card className={styles.entity}>
-              <h2>{pair[1]}</h2>
-              {this.link(pair[0], tag.tag)}
-            </Card>
-          ))}
-        </Flex>
-      </Content>
-    );
-  }
-}
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetch(tagId));
+  }, [dispatch, tagId]);
+
+  return (
+    <Content>
+      <Helmet title={tag.tag} />
+      <h1>{tag.tag}</h1>
+
+      <Flex wrap>
+        {map(toPairs(tag.relatedCounts), (pair) => (
+          <Card className={styles.entity}>
+            <h2>{pair[1]}</h2>
+            {link(pair[0], tag.tag)}
+          </Card>
+        ))}
+      </Flex>
+    </Content>
+  );
+};
 
 export default TagDetail;